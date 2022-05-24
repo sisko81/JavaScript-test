85c1a8d87bab8f19ea656dd82eb1160e845f44c8
sisko81 <abdurcampbell@yahoo.com>
2022-05-25 08:44:34 +0900

    Added multiply function

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 //Write a function called add7 that takes one number and returns that number + 7.
 
-function add7(num) {
-    return num + 7;
-}
+const add7 = (num) => num + 7;
 
-console.log(add7(4))
+console.log(add7(4));
+
+//Write a function called multiply that takes 2 numbers and returns their product.
+
+const multiply = (a, b) => a * b;
+
+console.log(multiply(7, 7))
