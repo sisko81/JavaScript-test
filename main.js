ff08629242261d33a3b9625e406c9b8f5e4519ce
sisko81 <abdurcampbell@yahoo.com>
2022-05-26 08:15:07 +0900

    exercise finished

--- main.js
+++ main.js
@@ -2,31 +2,33 @@
 
 const add7 = (num) => num + 7;
 
-console.log(add7(4));
+const result1 = add7(4)
+console.log(result1);
 
 //Write a function called multiply that takes 2 numbers and returns their product.
 
 const multiply = (a, b) => a * b;
 
-console.log(multiply(7, 7));
+const result2 = multiply(7, 7)
+console.log(result2);
 
 //Write a function called capitalize that takes a string and returns that string with only the first letter capitalized.
 // Make sure that it can take strings that are lowercase, UPPERCASE or BoTh.
 
-function capitalizeFirstLetter(str) {
+const capitalizeFirstLetter = (str) => {
 
     const capitalized = str.charAt(0).toUpperCase() + str.slice(1)
     return capitalized
 }
-const result = capitalizeFirstLetter("mariko")
-console.log(result);
+const result3 = capitalizeFirstLetter("mint")
+console.log(result3);
 
 //Write a function called lastLetter that takes a string and returns the very last letter of that string:
 
-function lastLetter(str2) {
+const lastLetter = (str2) => {
     const last = str2.slice(-1);
     return last
 }
 
-const result2 = lastLetter("abcd")
-console.log(result2)
+const result4 = lastLetter("abcd")
+console.log(result4)
